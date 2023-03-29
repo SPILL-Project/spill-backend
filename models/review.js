199929a786895462a18e20caabeeeec5ccbfd63c
Cicak Kayang <eriscapricron@gmail.com>
2023-03-30 03:18:34 +0700

    setup plus migration

---
+++ models/review.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  review.init({
+    user_id: DataTypes.UUID,
+    product_id: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    image: DataTypes.STRING,
+    rating: DataTypes.FLOAT,
+    description: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'review',
+  });
+  return review;
+};
