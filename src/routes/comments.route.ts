ad15ae36cef933b9280a5c83bd2710ebebd6248b
sslthn31 <alhambra.sulthan@gmail.com>
2023-07-05 21:24:50 +0700

    remove delete & put

--- src/routes/comments.route.ts
+++ src/routes/comments.route.ts
@@ -1,14 +1,12 @@
 import { Router } from 'express';
 import { createNewComment, deleteComment, getAllComments, updateComment } from '../controllers/comments.controller';
 
-// const middleware = require('../middleware');
-
 const commentsRouter: Router = Router();
 const middleware = require('../middleware');
 
 commentsRouter.get('/', getAllComments);
 commentsRouter.post('/', createNewComment);
-commentsRouter.delete('/', deleteComment);
-commentsRouter.put('/', updateComment);
+// commentsRouter.delete('/', deleteComment);
+// commentsRouter.put('/', updateComment);
 
 export default commentsRouter;
